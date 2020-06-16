d649677939260855471634924bc28f05452b1b52
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-06-16 17:54:49 +0100

    apply awesomebar patch

--- config.h
+++ config.h
@@ -20,7 +20,8 @@ static const char col_yellow[]      = "#ebcb8b";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_white, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_cyan, col_gray1,  col_cyan  },
+	[SchemeHid]  = { col_gray2,  col_gray1, col_cyan  },
 };
 
 /* key commands*/
@@ -134,6 +135,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkWinTitle,          0,              Button1,        togglewin,      {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
